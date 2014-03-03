f9127c04fbba0082a87390220561e2a99146b878
Ian Havelock <morrolan@me.com>
2014-03-03 13:09:08 +0000

    Added skeleton feramework for Robin script.

---
+++ robin.py
@@ -0,0 +1,20 @@
+__author__ = 'morrolan'
+
+import psutil
+import os
+
+def check_for_files():
+    pass
+
+
+def hide_oneself():
+    pass
+
+
+def check_for_antidote():
+    pass
+
+
+def save_friar():
+    pass
+
