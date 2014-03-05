f06f0ceeefa023108e1f92a0a6fa9aa58968098a
Ian Havelock <morrolan@me.com>
2014-03-05 10:22:03 +0000

    Added skeleton feramework for Robin script.

--- robin.py
+++ robin.py
@@ -4,17 +4,43 @@ import psutil
 import os
 
 def check_for_files():
+    """
+    Here we will do a cursory check for other viable instances of the program.
+    I was thinking of somehow writing copies of this file to randomly chosen locations
+    and then somehow making those locations available.
+    """
     pass
 
 
 def hide_oneself():
+    """
+    If I need to hide myself, i will do it here.  I am not not sure what form this will take.
+
+    """
     pass
 
 
 def check_for_antidote():
+    """
+    Here we will look for the antidote, which will be an empty file with a particular name.
+
+    """
     pass
 
 
 def save_friar():
+    """
+
+
+    """
     pass
 
+
+def talk_to_friar():
+    """
+    Here I will check to see if the Friar is running.  At the moment I don't know how to identify
+    a particular running process.  One possibility is to get the PID somehow and stash it somewhere
+    for the other brother process to pick up and monitor.
+
+    """
+    pass
