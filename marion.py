17f5e4eff95fbeac68986ee6ca9e75769467eb5a
Ian Havelock <ian@morrolan.com>
2014-03-24 17:48:03 +0000

    Tweaked things around so that robin and the friar are instances of sherwood.Merryman instead of spawning themselves.
    Marion.py is now in charge and is now the entry point.

--- marion.py
+++ marion.py
@@ -1,1 +1,12 @@
 __author__ = 'morrolan'
+
+import sherwood
+
+
+robin = sherwood.Merryman()
+robin.name = 'Robin'
+print robin.name
+robin.check_for_antidote('mead.txt', 'STOP')
+
+
+# friar = sherwood.Merryman()
--- sherwood.py
+++ sherwood.py
@@ -1,12 +1,59 @@
 __author__ = 'morrolan'
 
+import os
+
 
 class Merryman:
 
     name = ''
-    something_else = []
 
     def __init__(self):
+        pass
+
+    def check_for_files(self):
+        """
+        Here we will do a cursory check for other viable instances of the program.
+        I was thinking of somehow writing copies of this file to randomly chosen locations
+        and then somehow making those locations available.
+        """
+        pass
+
+
+    def hide_oneself(self):
+        """
+        If I need to hide myself, i will do it here.  I am not not sure what form this will take.
+
+        """
+        pass
+
+
+    def check_for_antidote(self, antidote_file, antidote_content):
+        """
+        Here we will look for the antidote, which will be a text file of a particular name with particular contents.
+
+        """
+        with open(os.path.normpath(antidote_file)) as __antidote_file:
+            __antidote_contents = __antidote_file.read()
+
+            if __antidote_contents == antidote_content:
+                print("STOP THE MERRYMEN FROM DOING THEIR SHIT HERE...")
+
+
+    def save_friar(self):
+        """
+        here we need to figure out and watch for the friar dying.
+
+        """
+        pass
+
+
+    def talk_to_friar(self):
+        """
+        Here I will check to see if the Friar is running.  At the moment I don't know how to identify
+        a particular running process.  One possibility is to get the PID somehow and stash it somewhere
+        for the other brother process to pick up and monitor.
+
+        """
         pass
 
 
