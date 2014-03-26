ac943743dcb230ffd3c0bd02f54da411e95166b1
Ian Havelock <ian@morrolan.com>
2014-03-26 00:36:00 +0000

    Expanded antidote and added more comments

--- marion.py
+++ marion.py
@@ -3,10 +3,12 @@ __author__ = 'morrolan'
 import sherwood
 
 
-robin = sherwood.Merryman()
-robin.name = 'Robin'
+robin = sherwood.Merryman('robin')
 print robin.name
-robin.check_for_antidote('mead.txta', 'STOP')
+antidote_present = robin.check_for_antidote('mead.txt', 'STOP')
+
+# Marion needs to be expanded so that instead of creating objects, she creates complete python scripts that are ran
+# via subprocess.call so that each instance, robin, friar or will, all have separate processes
 
 
-# friar = sherwood.Merryman()
+#friar = sherwood.Merryman('friar')
--- sherwood.py
+++ sherwood.py
@@ -1,14 +1,18 @@
 __author__ = 'morrolan'
 
 import os
+import random
 
 
-class Merryman:
+class Merryman(object):
 
-    name = ''
+    __all__ = []
 
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
 
     def check_for_files(self):
         """
@@ -18,7 +22,6 @@ class Merryman:
         """
         pass
 
-
     def hide_oneself(self):
         """
         If I need to hide myself, i will do it here.  I am not not sure what form this will take.
@@ -26,29 +29,30 @@ class Merryman:
         """
         pass
 
-
-    def check_for_antidote(self, antidote_file, antidote_content):
+    @staticmethod
+    def check_for_antidote(antidote_file, antidote_key):
         """
         Here we will look for the antidote, which will be a text file of a particular name with particular contents.
-
+        Marion should choose this when spawning the merrymen, and it should be obfuscated in some way that isn't easy
+        to find out retrospectively.
         """
         try:
-            with open(os.path.normpath(antidote_file)) as __antidote_file:
-                __antidote_contents = __antidote_file.read()
+            with open(os.path.normpath(antidote_file)) as _antidote_file:
+                _antidote_contents = _antidote_file.read()
 
-                if __antidote_contents == antidote_content:
+                if antidote_key in _antidote_contents:
                     print("STOP THE MERRYMEN FROM DOING THEIR SHIT HERE...")
+                    return True
+                else:
+                    return False
 
         except IOError:
-            print("Wench!  Why hath thou not brought forth more Meade?")
-
-
-    def save_friar(self):
-        """
-        here we need to figure out and watch for the friar dying.
-
-        """
-        pass
+            _result = random.randint(0, 3)
+            if _result == 0:
+                # would be nice to have a choice of sentences here.  Maybe create a list and have a random choice chosen
+                # and printed from it?
+                print("Wench!  Why hath thou not brought forth more Meade?")
+            return False
 
 
     def talk_to_friar(self):
@@ -57,6 +61,16 @@ class Merryman:
         a particular running process.  One possibility is to get the PID somehow and stash it somewhere
         for the other brother process to pick up and monitor.
 
+        """
+        pass
+
+
+    def save_friar(self):
+        """
+        here we need to figure out and watch for the friar dying.
+
+        Ideally it needs to be platform-agnostic, so using something like psutil module would be ideal, if I could get
+        it to install on OSX!
         """
         pass
 
